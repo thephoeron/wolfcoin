35612b6a892a6e0454525d26a0ed50b79cb29d83
the Phoeron <thephoeron@users.noreply.github.com>
2015-08-25 12:05:07 -0400

    Create hdk.lisp

---
+++ wallet/hdk.lisp
@@ -0,0 +1,11 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: WOLFCOIN-WALLET; Base: 10 -*-
+;;;; file: wallet/hdk.lisp
+
+;;;; Copyright (c) 2014--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; See LICENSE for additional information.
+
+(in-package :wolfcoin-wallet)
+
+
+
+;; EOF
