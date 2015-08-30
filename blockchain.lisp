21f69bbd70a33ae4472c359122b925a55c3a90ad
the Phoeron <thephoeron@users.noreply.github.com>
2015-08-30 11:55:41 -0400

    correct symbol collision on tx-block class and api

--- blockchain.lisp
+++ blockchain.lisp
@@ -14,7 +14,7 @@
   (let ((ctx (isaac:init-kernel-seed :is64 t)))
     (format nil "~64,'0x" (isaac:rand-bits-64 ctx 1024))))
 
-(defclass block ()
+(defclass tx-block ()
   ((id :initarg :id :reader id
        :documentation "The Hash-ID of the Block")
    (mint :initarg :mint :reader mint
@@ -26,22 +26,22 @@
    (transactions :initarg :transactions :accessor transactions
                  :documentation "The list of transactions on this block")))
 
-(defgeneric block-height (block)
+(defgeneric block-height (tx-block)
   (:documentation "Get the block-height of the current block on the blockchain."))
 
-(defgeneric number-of-transactions (block)
+(defgeneric number-of-transactions (tx-block)
   (:documentation "Count the transactions on the block."))
 
-(defgeneric valid-block-p (block)
+(defgeneric valid-block-p (tx-block)
   (:documentation "Block validator predicate."))
 
-(defgeneric longest-chain-p (block)
+(defgeneric longest-chain-p (tx-block)
   (:documentation "Ensure that current block is on the longest blockchain."))
 
-(defgeneric push-block-to-blockchain (block blockchain)
+(defgeneric push-block-to-blockchain (tx-block blockchain)
   (:documentation "Push new block to local blockchain."))
 
-(defgeneric push-tx-to-latest-block (tx block blockchain)
+(defgeneric push-tx-to-latest-block (tx tx-block blockchain)
   (:documentation "Find the latest block on the blockchain and push transaction to it."))
 
 ;; EOF
