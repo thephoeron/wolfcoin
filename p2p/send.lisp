45c20617f8b0ae5501a45fe2abf4828e9fceda56
the Phoeron <thephoeron@users.noreply.github.com>
2015-09-12 09:37:19 -0400

    Create p2p/send.lisp

---
+++ p2p/send.lisp
@@ -0,0 +1,9 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: WOLFCOIN; Base: 10 -*-
+;;;; file: p2p/send.lisp
+
+;;;; Copyright (c) 2014--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; See LICENSE for additional information.
+
+(in-package :wolfcoin-p2p)
+
+;; EOF
