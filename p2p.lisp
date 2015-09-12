20b14ba7ee79f9e6b4c93fc9d974d709b4f3c2d2
the Phoeron <thephoeron@users.noreply.github.com>
2015-09-12 09:41:40 -0400

    Delete p2p.lisp

--- p2p.lisp
+++
@@ -1,9 +0,0 @@
-;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: WOLFCOIN; Base: 10 -*-
-;;;; file: p2p.lisp
-
-;;;; Copyright (c) 2014--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
-;;;; See LICENSE for additional information.
-
-(in-package :wolfcoin)
-
-;; EOF
