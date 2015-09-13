a7df266c37f4c6e0d03443767fc1fe631adc4859
the Phoeron <thephoeron@users.noreply.github.com>
2015-09-13 08:52:51 -0400

    Create config.lisp

---
+++ config.lisp
@@ -0,0 +1,11 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: WOLFCOIN; Base: 10 -*-
+;;;; file: config.lisp
+
+;;;; Copyright (c) 2014--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; See LICENSE for additional information.
+
+(in-package :wolfcoin)
+
+;; - ensure directory ~/.wolfcoin exists
+
+;; EOF
