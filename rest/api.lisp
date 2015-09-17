af3ef60725e968c7ff939ad74d73776416f14335
the Phoeron <thephoeron@users.noreply.github.com>
2015-09-17 09:02:46 -0400

    Create rest/api.lisp

---
+++ rest/api.lisp
@@ -0,0 +1,11 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: WOLFCOIN-API; Base: 10 -*-
+;;;; file: rest/api.lisp
+
+;;;; Copyright (c) 2014--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; See LICENSE for additional information.
+
+(in-package :wolfcoin-api)
+
+
+
+;; EOF
